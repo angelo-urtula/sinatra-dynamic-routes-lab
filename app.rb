f511701bb300e0107fb7165885cea97590cd827d
angelo <aurtula1@gmail.com>
2021-04-16 05:44:25 -0400

    commit tree 7c8a620e28f7405aa67cb33539c9c940053b4b77

--- app.rb
+++ app.rb
@@ -2,5 +2,38 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get "/reversename/:name" do
+    @reversed_name = params[:name].reverse
+    "#{@reversed_name}"
+  end
 
-end
+  get "/square/:number" do
+    @squared = params[:number].to_i * params[:number].to_i
+    "#{@squared}"
+  end
+
+  get "/say/:number/:phrase" do
+    @phrase = params[:phrase]
+    "#{@phrase}" * params[:number].to_i
+  end
+
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    "#{@params[:word1]} #{@params[:word2]} #{@params[:word3]} #{@params[:word4]} #{@params[:word5]}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    if params[:operation] == "add"
+      @sum = params[:number1].to_i + params[:number2].to_i
+      "#{@sum}"
+    elsif params[:operation] == "subtract"
+      @diff = params[:number1].to_i - params[:number2].to_i
+      "#{@diff}"
+    elsif params[:operation] == "multiply"
+      @product = params[:number1].to_i * params[:number2].to_i
+      "#{@product}"
+    elsif params[:operation] == "divide"
+      @quotient = params[:number1].to_i / params[:number2].to_i
+      "#{@quotient}"
+    end
+  end
+end 
